b0c04be65cebb2641688a616e2eda311b3ce64e9
SAPIENT\kansar <kansari@sapient.com>
2017-05-25 23:22:24 +0530

    removed unwanted code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   // title = 'app works!';
-  private chartData: Array<any>;
+
   ngOnInit() {
-   // this.generateData();
-  }
-  generateData() {
-    this.chartData = [];
-    for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
-      this.chartData.push([
-        `Index ${i}`,
-        Math.floor(Math.random() * 100)
-      ]);
-    }
+   
   }
 }
